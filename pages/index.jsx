b4b5d21e6100af22b32154bfb37afda9ea1a7216
Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>
2023-03-03 15:22:48 +0530

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -90,7 +90,7 @@ Vote Astra</a>
                   <p className="text-3xl text-white font-semibold">Best Your Bot</p>
                   <p className="text-md text-white font-medium text-gray-500 line-clamp-5">Are you ready to throw the best parties with Astra, the best and most up-to-date bot?</p>
                 </div>
-                <img src="https://cdn.discordapp.com/attachments/737348411568685066/983094813115170866/unknown.png" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
+                <img src="https://www.linkpicture.com/q/Capture_449.png" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
               </>
       
         </div>
