22f548c89f8b72d52523e108b811cf87262eb4e2
Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>
2023-03-03 15:01:51 +0530

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -37,7 +37,7 @@ export default function AwardApp({ Component, pageProps }) {
       name: "Support",
       icon: "fab fa-discord",
       activeIcon: "fab fa-discord",
-      href: "https://discord.gg/ZcDprCrFTn",
+      href: "https://discord.gg/JaNCZvPxMQ",
     },
     {
       link: true,
