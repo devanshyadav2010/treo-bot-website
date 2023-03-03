d0edfaf6f6657a270b8282b7a4752de7a9e8e410
Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>
2023-03-03 16:01:23 +0530

    Update privacy.jsx

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -2,9 +2,10 @@ export default function Privacy() {
 
     return (
         <div>
-            <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for Astra</h1>
+            <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for Treo</h1>
             <p className="text-sm text-white/30 mb-4">Last Update: 20/05/2022  🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
+        <h2 className="font-semibold text-3xl text-white/75">Usage Of Data</h2>
         <p>⊳ To use the Astra bot, you must define the necessary permissions when adding it to your discord server. <a className="text-white">(READ MESSAGES, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
         <div className="py-3"></div>
         <p>⊳ You have to vote for the <a className="text-white" href="https://top.gg/bot/924325576095973426/vote">Astra</a> bot when using certain commands from the bot commands.</p>
