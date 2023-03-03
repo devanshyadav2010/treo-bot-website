a6d0c0e404c44b4206b10aa7609071995e3fbe46
Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>
2023-03-03 15:39:28 +0530

    Update tos.jsx

--- pages/tos.jsx
+++ pages/tos.jsx
@@ -3,7 +3,7 @@ export default function Tos() {
     <div>
      <h2 className="mt-4 text-white font-bold text-3xl">Terms of Services</h2>
       <p className="text-sm text-white/30 mb-1">Last Update: 20/05/2022 🆕️</p>
-  <div className="animateHeader text-white text-opacity-40 mt-5">
+  <div className="animateHeader text-white text-opacity-40 mt-5"><h1>Usage Agreement</h1>
 <p>⊳ Every discord server can use the Astra bot for free, it is completely prohibited to sell or market for a fee.</p>
 <div className="py-3"></div>
 <p>⊳ You cannot use bot commands to harm the bot.</p>
