aa9803f3040032a4e266ff6f8af1b55080b78e00
Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>
2023-03-03 16:28:19 +0530

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -29,7 +29,7 @@ export default function Index() {
   <div className="py-10"></div>
   <Link href={"https://top.gg/bot/1016392200516550736/vote"}>
   <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
-Vote Astra</a>
+Vote Treo</a>
   </Link>
         </div>
 
