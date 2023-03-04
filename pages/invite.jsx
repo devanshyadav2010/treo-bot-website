34dbc1fb845114620776bb9cb3531b8e20bb39ca
Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>
2023-03-04 19:54:29 +0530

    Update invite.jsx
    Signed-off-by: Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>

--- pages/invite.jsx
+++ pages/invite.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 
 const RedirectPage = () => {
-  return <Redirect to="https://treo.vercel.app/" />;
+  return <Redirect to="https://discord.com/oauth2/authorize?client_id=1016392200516550736&permissions=2146958591&scope=bot%20applications.commands/" />;
 };
 
 export default RedirectPage;
