52a78845244c427b689488e0935f1b373700e243
Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>
2023-03-04 17:46:28 +0530

    Create invite.jsx
    Signed-off-by: Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>

---
+++ pages/invite.jsx
@@ -0,0 +1,39 @@
+export default function Privacy() {
+
+    return (
+        <div>
+            <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for Treo</h1>
+            <p className="text-sm text-white/30 mb-4">Last Update: 20/05/2022  🆕️</p>
+  <div className="animateHeader text-white text-opacity-40 mt-5">
+        <h2 className="font-semibold text-3xl text-white/75">Usage Of Data</h2>
+        <p>⊳ The bot may use stored data for different purposes, including but not limited to:
+
+Storing the guild-specific settings of the bot (Corresponding commands)
+Providing the command handling of the bot
+Feature usage
+No usage of data outside the aforementioned cases will happen and under no circumstances will the data be shared with third parties</p>
+        <div className="py-3"></div>
+    <h2 className="font-semibold text-3xl text-white/75">Stored Information</h2>
+        <p>⊳ The bot may store the following information upon a User using Certain Commands:
+
+-Their Database is stored within mongodb and kept privately in my files
+
+No other information outside the above-mentioned one will be stored AUTOMATICALLY.</p>
+        <div className="py-3"></div>
+    <h2 className="font-semibold text-3xl text-white/75">Updating Data</h2>
+        <p>⊳ The data may be updated when using specific commands. Updating data will require the input of an end user, and data that can be seen as sensitive, such as content of a message, may need to be stored when using certain commands.
+
+No other actions may update the stored information at any given time.</p>
+        <div className="py-3"></div>
+    <h2 className="font-semibold text-3xl text-white/75">Removal Of Data</h2>
+        <h1 className="font-semibold text-3xl text-white/75">Automatic Removal</h1>
+        <p>⊳ Stored Data can be removed automatically through means of removing the bot from a Server. This can be achieved by either kicking or banning the bot from the server. Re-inviting the bot will add the same default values, as mentioned above, back to the bot's' database..</p>   
+        <div className="py-3"></div>
+    <h1 className="font-semibold text-3xl text-white/75">Manual Removal</h1>
+        <p>⊳ Manual removal of the data can be requested by contacting the developer DEVANSH#4075 with the subject 'Removal of Yuzu Data'. For security reasons will we ask you to provide us with proof of ownership of the server, that you wish the data to be removed of. Only a server owner may request manual removal of data and requesting it will result in the bot being removed from the server, if still present on it.</p>
+       
+    
+        </div>
+            </div>
+    );
+};
