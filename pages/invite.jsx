267f6954ae6910bf7ef8d02289fc5fd3f3ce1f51
Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>
2023-03-04 20:07:58 +0530

    Update invite.jsx
    Signed-off-by: Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>

--- pages/invite.jsx
+++ pages/invite.jsx
@@ -1,51 +1,54 @@
-import React from "react"; 
-// importing Link from react-router-dom to navigate to  
-// different end points. 
 
-import { Link } from "react-router-dom"; 
-
-  
 export default function Invite() {
 
-  return ( 
+    return (
+        <>
+            <div className="w-full my-10">
+            <div className="flex w-full justify-center">
+      <img width="400" src="/img/bck4.png" />
+      </div>
+                <h1 className="py-7 animateHeader text-4xl font-extrabold text-center text-white">
+                    <i className="fal fa-stars text-amber-400 mr-2" />
+                    Partners
+                </h1>
+            </div>
 
-    <div> 
+            <div className="lg:max-w-screen-lg mt-10 mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
 
-      <h1>Home Page</h1> 
+            <div className="flex flex-col justify-center text-white rounded">
+            <a href="https://discord.gg/6XGqdgE">
+                        <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
+                            <img className="rounded-full h-32 w-32" src="https://cdn.discordapp.com/icons/676124992919830548/68ef946cf7cc39e4063eed4d446f6dda.webp" />
+                            <div>
+                                <h1 className="leading-none text-3xl font-bold text-white">Code Share</h1>        
+                                <div className="flex items-center mt-1">
+                                    <p className="font-normal font-sm">
+                                        Share Your Codes, Can You Keep Your Projects Active Forever. World's Largest Website with Discord Interface!
+                                    </p>
+                                </div>     
+                            </div>
+                        </div>
+                    </a>
+                    </div>
 
-      <br /> 
+                    <div className="flex flex-col justify-center text-white rounded">
+                        <a href="https://discord.gg/Fy4WmSThRZ">
+                            <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
+                            <img className="rounded-full h-32 w-32" src="https://cdn.discordapp.com/icons/924312493537509407/721de16575a9048c9c6980613ebb99f0.webp" />
+                            <div>
+                                <h1 className="leading-none text-3xl font-bold text-white">The Developers</h1>        
+                                <div className="flex items-center mt-1">
+                                    <p className="font-normal font-sm">
+                                    You can get help on software and similar issues or you can spend time chatting. How about being a cool developer?
+                                    </p>
+                                </div>     
+                            </div>
+                        </div>
+                        </a>
+                        </div>
 
-      <ul> 
-
-        <li> 
-
-          {/* Endpoint to route to Home component */} 
-
-          <Link to="/">Home</Link> 
-
-        </li> 
-
-        <li> 
-
-          {/* Endpoint to route to About component */} 
-
-          <Link to="/about">About</Link> 
-
-        </li> 
-
-        <li> 
-
-          {/* Endpoint to route to Contact Us component */} 
-
-          <Link to="/contactus">Contact Us</Link> 
-
-        </li> 
-
-      </ul> 
-
-    </div> 
-
-  ); 
-}; 
-
- 
+                </div>
+                <div className="py-10"></div>
+        </>
+    );
+};
