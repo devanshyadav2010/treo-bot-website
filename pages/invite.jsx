7bd0c91f4fb748917e5e346ad2c927a35ade7b06
Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>
2023-03-04 20:13:31 +0530

    Delete invite.jsx
    Signed-off-by: Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>

--- pages/invite.jsx
+++
@@ -1,8 +0,0 @@
-
-export default function Invite() {
-
-    return (
-        return <Redirect to="https://discord.com/oauth2/authorize?client_id=1016392200516550736&permissions=2146958591&scope=bot%20applications.commands"/>;
-        
-    );
-};
