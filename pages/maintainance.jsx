9576ab644d5f77c5291035e84a15709583998dcc
Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>
2023-03-05 02:11:49 +0530

    Update maintainance.jsx
    Signed-off-by: Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>

--- pages/maintainance.jsx
+++ pages/maintainance.jsx
@@ -2,8 +2,8 @@ export default function Privacy() {
 
     return (
         <div>
-            <h1 className="font-semibold text-3xl text-white/75">This Page Is Under Maintenance</h1>
-            
+            <center><h1 className="font-semibold text-3xl text-white/75">This Page Is Under Maintenance</h1>
+            </center>
   <div className="animateHeader text-white text-opacity-40 mt-5">
      
         </div>
