59a7df9fef76260dda03e84b7f69ee344d689ab0
Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>
2023-03-04 19:57:56 +0530

    Update invite.jsx
    Signed-off-by: Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>

--- pages/invite.jsx
+++ pages/invite.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 
-const RedirectPage = () => {
+const Invite = () => {
   return <Redirect to="https://discord.com/oauth2/authorize?client_id=1016392200516550736&permissions=2146958591&scope=bot%20applications.commands/" />;
 };
 
