9c576da5ca748bc86f3d47e8068f7a51d0f3ed20
Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>
2023-03-04 20:12:31 +0530

    Update invite.jsx
    Signed-off-by: Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>

--- pages/invite.jsx
+++ pages/invite.jsx
@@ -2,7 +2,7 @@
 export default function Invite() {
 
     return (
-        return <Redirect to="https://discord.com/oauth2/authorize?client_id=1016392200516550736&permissions=2146958591&scope=bot%20applications.commands" />;
+        return <Redirect to="https://discord.com/oauth2/authorize?client_id=1016392200516550736&permissions=2146958591&scope=bot%20applications.commands"/>;
         
     );
 };
