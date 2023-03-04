ce3dc42d46386630a884f82544eff058af6443d7
Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>
2023-03-05 02:04:39 +0530

    Update maintainance.jsx
    Signed-off-by: Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>

--- pages/maintainance.jsx
+++ pages/maintainance.jsx
@@ -2,7 +2,7 @@ export default function maintainance() {
 
     return (
         <div>
-         <h1>THIS PAGE IS IN MAINTENANCE</ha>
+         <h1 className="font-semibold text-3xl text-white/75">THIS PAGE IS IN MAINTENANCE</ha>
             </div>
     );
 };
