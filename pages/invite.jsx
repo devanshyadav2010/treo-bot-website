a080df31dcb89793bde380847f8787d88f713b91
Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>
2023-03-05 11:02:19 +0530

    Create invite.jsx
    Signed-off-by: Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>

---
+++ pages/invite.jsx
@@ -0,0 +1,13 @@
+export default function invite() {
+
+    return (
+        <div>
+            <center><h1 className="font-semibold text-3xl text-white/75">INVITE</h1>
+            </center>
+  <div className="animateHeader text-white text-opacity-40 mt-5">
+     
+        </div>
+        
+            </div>
+    );
+};
