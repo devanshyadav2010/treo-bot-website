16b06dd0b74770523feb431b07b08778ed1c3166
Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>
2023-03-05 14:01:42 +0530

    Delete invite.jsx
    Signed-off-by: Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>

--- pages/invite.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Redirect } from 'react-router-dom';
-
-export default funtion invite() {
-  return ( 
-<Redirect to="https://treo.vercel.app/partners" />;
-)
-};
