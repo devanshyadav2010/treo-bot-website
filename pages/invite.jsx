ba19d40017800811755a9bece9e46903ed760205
Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>
2023-03-05 11:18:21 +0530

    Update invite.jsx
    Signed-off-by: Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>

--- pages/invite.jsx
+++ pages/invite.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 
 export default funtion invite() {
-  return <Redirect to="https://treo.vercel.app/partners" />;
+  return ( 
+<Redirect to="https://treo.vercel.app/partners" />;
+)
 };
-
-export default invite;
