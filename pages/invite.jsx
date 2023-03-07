950d73d8b1de25ea251e3d499e7f8e68065692de
Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>
2023-03-07 18:33:33 +0530

    Update invite.jsx
    Signed-off-by: Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>

--- pages/invite.jsx
+++ pages/invite.jsx
@@ -7,7 +7,7 @@ export default function invite() {
 }
 
 export async function getServerSideProps({ res }) {
-  res.writeHead(302, { Location: 'https://top.gg/' });
+  res.writeHead(302, { Location: 'https://discord.com/oauth2/authorize?client_id=1016392200516550736&permissions=2146958591&scope=bot%20applications.commands' });
   res.end();
 
   return {
