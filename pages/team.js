eb66a954a4753ccede48b90fc863cc10b300b40e
Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>
2023-03-22 23:02:47 +0530

    Update team.js
    Signed-off-by: Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>

--- pages/team.js
+++ pages/team.js
@@ -1,43 +1,11 @@
-import React from 'react';
-
-const teamMembers = [
-  {
-    name: 'John Doe',
-    role: 'Owner',
-    avatarUrl: 'https://images-ext-1.discordapp.net/external/ZVEvKf45XxQctxcaiYCfWFGzgI6W8yXdrKAhS5I9uk4/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/980069165459198022/283716454cf02f9c2083375779b730af.png',
-  },
-  {
-    name: 'Jane Doe',
-    role: 'Developer',
-    avatarUrl: 'https://images-ext-1.discordapp.net/external/ZVEvKf45XxQctxcaiYCfWFGzgI6W8yXdrKAhS5I9uk4/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/980069165459198022/283716454cf02f9c2083375779b730af.png',
-  },
-  {
-    name: 'Alex Smith',
-    role: 'Manager',
-    avatarUrl: 'https://images-ext-1.discordapp.net/external/ZVEvKf45XxQctxcaiYCfWFGzgI6W8yXdrKAhS5I9uk4/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/980069165459198022/283716454cf02f9c2083375779b730af.png',
-  },
-];
-
-const TeamMember = ({ name, role, avatarUrl }) => (
-  <div>
-    <img src={avatarUrl} alt={`${name}'s avatar`} />
-    <h3>{name}</h3>
-    <p>{role}</p>
-  </div>
-);
+import TreoTeam from '../components/team';
 
 export default function Team() {
   return (
     <div>
-      <h1>Meet the Team</h1>
-      {teamMembers.map((member) => (
-        <TeamMember
-          key={member.name}
-          name={member.name}
-          role={member.role}
-          avatarUrl={member.avatarUrl}
-        />
-      ))}
+      <h1>About us</h1>
+      <p>We are a team of developers, managers, and owners.</p>
+      <TreoTeam />
     </div>
   );
 }
