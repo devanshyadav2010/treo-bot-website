95850ba83f444a60360a867cdf504747625a8a7d
Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>
2023-04-08 18:08:40 +0530

    Update commands.jsx
    Signed-off-by: Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>

--- pages/commands.jsx
+++ pages/commands.jsx
@@ -1,6 +1,18 @@
 import { useState } from 'react';
 
-const categories = [  {    name: "Information",    commands: [      {        name: "Help",        description: "Get help with using the bot."      },      {        name: "User",        description: "Get information about a user."      }    ]
+const categories = [
+  {
+    name: "Information",
+    commands: [
+      {
+        name: "Help",
+        description: "Get help with using the bot."
+      },
+      {
+        name: "User",
+        description: "Get information about a user."
+      }
+    ]
   },
   {
     name: "Economy",
@@ -78,30 +90,29 @@ export default function Commands() {
 
   return (
     <div className="container mx-auto mt-10">
-      <div className="flex flex-wrap flex-col md:flex-wrap md:-mx-2">
-  {categories.map((category) => (
-    <div key={category.name} className="md:w-1/3 md:px-2 mb-4">
-      <button
-        className={`w-full h-12 shadow-lg shadow-amber-600/20 rounded-md py-2 px-2 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white ${selectedCategory.name === category.name ? 'bg-amber-600' : ''}`}
-        onClick={() => handleCategoryClick(category)}
-      >
-        {category.name}
-      </button>
-    </div>
-  ))}
-</div>
+      <div className="flex flex-wrap flex-col md:flex-row md:-mx-2">
+        {categories.map((category) => (
+          <div key={category.name} className="md:w-1/3 md:px-2 mb-4">
+            <button
+              className={`w-full h-12 shadow-lg shadow-amber-600/20 rounded-md py-2 px-2 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white ${selectedCategory.name === category.name ? 'bg-amber-600' : ''}`}
+              onClick={() => handleCategoryClick(category)}
+            >
+              {category.name}
+            </button>
+          </div>
+        ))}
+      </div>
 
-      
       <div className="flex flex-wrap mt-10">
         {selectedCategory.commands.map((command) => (
           <div key={command.name} className="w-full md:w-1/2 lg:w-1/3 px-2 mb-4">
             <div className="h-full bg-white shadow-md rounded-md px-4 py-6 hover:shadow-lg transition duration-200">
-<h2 className="text-lg font-bold mb-2">{command.name}</h2>
-<p className="text-gray-600">{command.description}</p>
-</div>
-</div>
-))}
-</div>
-</div>
-);
+              <h2 className="text-lg font-bold mb-2">{command.name}</h2>
+              <p className="text-gray-600">{command.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
