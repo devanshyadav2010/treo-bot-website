19601b7921fff022ebc7ed455bd008e77e065b81
Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>
2023-04-08 16:54:31 +0530

    Update commands.jsx
    Signed-off-by: Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>

--- pages/commands.jsx
+++ pages/commands.jsx
@@ -4,7 +4,126 @@ const categories = [
   {
     name: "Information",
     commands: [
+      {import { useState } from 'react';
+
+const categories = [
+  {
+    name: "Information",
+    commands: [
       {
+        name: "Help",
+        description: "Get help with using the bot."
+      },
+      {
+        name: "User",
+        description: "Get information about a user."
+      }
+    ]
+  },
+  {
+    name: "Economy",
+    commands: [
+      {
+        name: "Servers",
+        description: "Get information about the servers the bot is in."
+      },
+      {
+        name: "Members",
+        description: "Get information about the members in a server."
+      }
+    ]
+  },
+  {
+    name: "Utility",
+    commands: [
+      {
+        name: "Ping",
+        description: "Get the latency of the bot."
+      },
+      {
+        name: "Poll",
+        description: "Create a poll for members to vote on."
+      }
+    ]
+  },
+  {
+    name: "Antinuke",
+    commands: [
+      {
+        name: "Ban",
+        description: "Ban a user from a server."
+      },
+      {
+        name: "Kick",
+        description: "Kick a user from a server."
+      }
+    ]
+  },
+  {
+    name: "Ticket",
+    commands: [
+      {
+        name: "Open",
+        description: "Open a support ticket."
+      },
+      {
+        name: "Close",
+        description: "Close a support ticket."
+      }
+    ]
+  },
+  {
+    name: "Welcome",
+    commands: [
+      {
+        name: "Set",
+        description: "Set the welcome message for a server."
+      },
+      {
+        name: "Reset",
+        description: "Reset the welcome message for a server."
+      }
+    ]
+  }
+];
+
+export default function Commands() {
+  const [selectedCategory, setSelectedCategory] = useState(categories[0]);
+
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+  }
+
+  return (
+    <div className="container mx-auto mt-10">
+      <div className="flex flex-wrap">
+        {categories.map((category) => (
+          <div key={category.name} className="w-full md:w-1/2 lg:w-1/3 px-2 mb-4">
+            <button
+              className={`w-full shadow-lg shadow-amber-600/20 rounded-md py-2 px-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white ${selectedCategory.name === category.name ? 'bg-amber-600' : ''}`}
+              onClick={() => handleCategoryClick(category)}
+            >
+              {category.name}
+            </button>
+          </div>
+        ))}
+      </div>
+      
+      <div className="flex flex-wrap mt-10 relative">
+        <div className="absolute top-0 left-0 w-full h-full border-red-500 border-4 border-solid rounded-lg z-10 animate-border-red"></div>
+        {selectedCategory.commands.map((command) => (
+          <div key={command.name} className="w-full md:w-1/2 lg:w-1/3 px-2 mb-4">
+            <div className="bg-white border border-gray-200 rounded-lg p-4 h-full">
+              <div className="font-medium mb-2">{command.name}</div>
+              <div className="text-gray-500">{command.description}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
         name: "Help",
         description: "Get help with using the bot."
       },
