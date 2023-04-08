15f0b02e2bd3a3e954e3a246bce5f8005eb2674e
Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>
2023-04-08 19:25:32 +0530

    Update index.jsx
    Signed-off-by: Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,100 +1,116 @@
-import Link from "next/link";
-import Image from "next/image";
+import { useState } from "react";
 
-export default function Index() {
-	
+const commandCategories = [  {    name: "Information",    description: "Commands that provide information about the server or its members.",    commands: [      {        name: "!help",        description: "Display a list of available commands.",      },      {        name: "!info",        description: "Display information about the server.",      },      {        name: "!rules",        description: "Display the server's rules.",      },    ],
+  },
+  {
+    name: "Economy",
+    description: "Commands that relate to the server's economy or currency system.",
+    commands: [
+      {
+        name: "!balance",
+        description: "Display your account balance.",
+      },
+      {
+        name: "!shop",
+        description: "Display the server's shop.",
+      },
+      {
+        name: "!buy [item]",
+        description: "Buy an item from the server's shop.",
+      },
+    ],
+  },
+  {
+    name: "Utility",
+    description: "Commands that provide utility functions, such as time zone conversions or weather reports.",
+    commands: [
+      {
+        name: "!timezone [location]",
+        description: "Display the current time in a specified time zone.",
+      },
+      {
+        name: "!weather [location]",
+        description: "Display the current weather in a specified location.",
+      },
+    ],
+  },
+  {
+    name: "Antinuke",
+    description: "Commands that protect the server from malicious actions or spam.",
+    commands: [
+      {
+        name: "!ban [user] [reason]",
+        description: "Ban a user from the server.",
+      },
+      {
+        name: "!kick [user] [reason]",
+        description: "Kick a user from the server.",
+      },
+      {
+        name: "!mute [user] [duration] [reason]",
+        description: "Mute a user for a specified duration.",
+      },
+    ],
+  },
+  {
+    name: "Ticket",
+    description: "Commands that create or manage support tickets.",
+    commands: [
+      {
+        name: "!newticket [issue]",
+        description: "Create a new support ticket with a specified issue.",
+      },
+      {
+        name: "!closeticket [ticket]",
+        description: "Close a support ticket with a specified ID.",
+      },
+    ],
+  },
+  {
+    name: "Welcome",
+    description: "Commands that welcome new users to the server or provide introductions.",
+    commands: [
+      {
+        name: "!welcome [user]",
+        description: "Send a welcome message to a new user.",
+      },
+      {
+        name: "!intro",
+        description: "Introduce yourself to the server.",
+      },
+    ],
+  },
+];
+
+export default function Commands() {
+  const [currentCategory, setCurrentCategory] = useState(commandCategories[0]);
+
   return (
-    <>
-      <div className="py-20 mb-30 max-w-3xl mx-auto">
-        <div className="flex w-full justify-center">
-      <img width="500" src="/img/bck.png" />
+    <div>
+      <div className="py-10 flex justify-center">
+        {commandCategories.map((category) => (
+          <button
+            key={category.name}
+            className={`mx-2 px-6 rounded-md py-2 font-medium ${
+              currentCategory.name === category.name
+                ? "bg-gradient-to-bl from-amber-700 to-amber-500 text-white shadow-lg shadow-amber-600/20"
+                : "bg-gray-200 text-gray-600 hover:bg-gray-300"
+            }`}
+            onClick={() => setCurrentCategory(category)}
+          >
+            {category.name}
+          </button>
+        ))}
       </div>
-        <p className="animateHeader text-4xl font-extrabold text-center text-white">
-          Treo
-        </p>
-        <p className="animateHeader text-white text-opacity-50 text-center mt-5">
-      Treo is a multipurpose bot for your discord server with Moderation, Fun, Giveaway, Economy, Utility etc... features. 
-	  </p>
-        <div className="animateHeader mt-10 flex flex-wrap items-center justify-center gap-x-4">
-  
-            <Link href={"https://discord.com/oauth2/authorize?client_id=1016392200516550736&permissions=2146958591&scope=bot%20applications.commands"}>
-              <a
-                className={
-                  "flex items-center px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " 
-                }
-              >
-          Invite Treo
-              </a>
-            </Link>
-  <div className="py-10"></div>
-  <Link href={"https://top.gg/bot/1016392200516550736/vote"}>
-  <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
-Vote Treo</a>
-  </Link>
-        </div>
-
-        <div style={{ zIndex: '-1' }} className="hidden xl:block undrag pointer-events-none opacity-10 -left-[10vw] absolute top-[8vw] h-[35vw] flex-shrink-0 grayscale rotate-[1deg]">
-        <Image width="600" height="600" src="/img/gift-left.png"  />
-        </div>
-        <div style={{ zIndex: '-1' }} className="hidden xl:block undrag opacity-10 pointer-events-none -right-[15vw] absolute top-[13vw] h-[90vw] grayscale -rotate-9">
-        <Image  width="600" height="600" src="/img/gift-right.png"  />
-        </div>
-      </div>
-
-
-
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-y-12 lg:gap-y-0 lg:gap-x-12 py-24">
-          <div className="col-span-4">
-            <p className="text-white text-4xl font-bold" dangerouslySetInnerHTML={{ __html:"Treo Bot" }} />
-            <p className="text-white text-md font-medium text-gray-500/75" dangerouslySetInnerHTML={{ __html: "How about creating the best environment to relax yourself with your own emotions?" }}/>
-            <a href="https://discord.com/oauth2/authorize?client_id=1016392200516550736&permissions=2146958591&scope=bot%20applications.commands" className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white"}>
-              <i className="fab fa-discord mr-2" />Invite Treo
-            </a>
+      <div className="px-10">
+        <h2 className="text-3 {currentCategory.commands.map((command) => (
+          <div key={command.name} className="py-2">
+            <div className="text-lg font-medium">{command.name}</div>
+            <div className="text-sm text-gray-500">{command.description}</div>
           </div>
-
-          <div className="col-span-8 grid grid-cols-1 lg:grid-cols-2 gap-6">
-           
-              <div>
-                <div className="text-white">
-                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-cogs text-2xl text-amber-500`} /> Optimization</p>
-                  <p className="text-gray-500 line-clamp-4">With fast and customizable settings specific to your server, A gives you a musical pleasure to the fullest.</p>
-                </div>
-              </div>
-              <div>
-                <div className="text-white">
-                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-universal-access text-2xl text-amber-500`} /> Universal</p>
-                  <p className="text-gray-500 line-clamp-4">Treo is a multipurpose bot for your discord server with Moderation, Fun, Giveaway, Economy, Utility etc... features.</p>
-                </div>
-              </div>
-              <div>
-                <div className="text-white">
-                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-shield text-2xl text-amber-500`} /> Security</p>
-                  <p className="text-gray-500 line-clamp-4">An open source safe bot that you can browse and help us develop(Source code soon).</p>
-                </div>
-              </div>
-              <div>
-                <div className="text-white">
-                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-ticket text-2xl text-amber-500`} /> Support</p>
-                  <p className="text-gray-500 line-clamp-4">With our best teammates, we provide the fastest response to anyone who needs help.</p>
-                </div>
-              </div>
-          </div>
+        ))}
       </div>
-
-
-
-      <div className="flex flex-row w-full h-auto lg:h-56 my-14 lg:my-40 bg-gradient-to-br lg:px-30 from-neutral-900/90 flex items-center justify-start to-neutral-900/50 rounded-lg p-1 shadow-md">
-              <>
-                <div className="lg:mx-4 lg:p-8 py-4 lg:py-16 flex items-center justify-center flex-col text-center w-full lg:w-auto lg:text-left lg:flex-none">
-                  <img src="https://www.linkpicture.com/q/Capture_449.png"  width="256" className="lg:hidden rounded-xl mb-5 shadow-xl shadow-black" />
-                  <p className="text-3xl text-white font-semibold">Best Your Bot</p>
-                  <p className="text-md text-white font-medium text-gray-500 line-clamp-5">Are you ready to throw the best parties with Treo, the best and most up-to-date bot?</p>
-                </div>
-                <img src="https://www.linkpicture.com/q/Capture_449.png" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
-              </>
-      
-        </div>
-
-      </>
-  )
+    </div>
+  );
 }
+
