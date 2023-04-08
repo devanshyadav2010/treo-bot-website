42338faa8e60ba47a5d37bdebd22a6bc0686b648
Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>
2023-04-08 19:44:23 +0530

    Update commands.jsx
    Signed-off-by: Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>

--- pages/commands.jsx
+++ pages/commands.jsx
@@ -1,118 +1,48 @@
-import { useState } from 'react';
+import React from 'react';
 
-const categories = [
-  {
-    name: "Information",
-    commands: [
-      {
-        name: "Help",
-        description: "Get help with using the bot."
-      },
-      {
-        name: "User",
-        description: "Get information about a user."
-      }
-    ]
+const categories = [  {    name: "Information",    commands: [      { name: "Help", description: "Get help with using the bot." },      { name: "User", description: "Get information about a user." }    ]
   },
   {
     name: "Economy",
     commands: [
-      {
-        name: "Servers",
-        description: "Get information about the servers the bot is in."
-      },
-      {
-        name: "Members",
-        description: "Get information about the members in a server."
-      }
+      { name: "Servers", description: "Get information about the servers the bot is in." },
+      { name: "Members", description: "Get information about the members in a server." }
     ]
   },
   {
     name: "Utility",
     commands: [
-      {
-        name: "Ping",
-        description: "Get the latency of the bot."
-      },
-      {
-        name: "Poll",
-        description: "Create a poll for members to vote on."
-      }
-    ]
-  },
-  {
-    name: "Antinuke",
-    commands: [
-      {
-        name: "Ban",
-        description: "Ban a user from a server."
-      },
-      {
-        name: "Kick",
-        description: "Kick a user from a server."
-      }
-    ]
-  },
-  {
-    name: "Ticket",
-    commands: [
-      {
-        name: "Open",
-        description: "Open a support ticket."
-      },
-      {
-        name: "Close",
-        description: "Close a support ticket."
-      }
-    ]
-  },
-  {
-    name: "Welcome",
-    commands: [
-      {
-        name: "Set",
-        description: "Set the welcome message for a server."
-      },
-      {
-        name: "Reset",
-        description: "Reset the welcome message for a server."
-      }
+      { name: "Ping", description: "Get the latency of the bot." },
+      { name: "Poll", description: "Create a poll for members to vote on." }
     ]
   }
 ];
 
-export default function Commands() {
-  const [selectedCategory, setSelectedCategory] = useState(categories[0]);
-
-  const handleCategoryClick = (category) => {
-    setSelectedCategory(category);
-  }
-
+const Table = () => {
   return (
-    <div className="container mx-auto mt-10">
-      <div className="flex flex-wrap flex-col md:flex-row">
-        {categories.map((category) => (
-          <div key={category.name} className="md:w-1/3 md:px-2 mb-4">
-            <button
-              className={`w-full md:w-auto h-12 shadow-lg shadow-amber-600/20 rounded-md py-2 px-2 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white ${selectedCategory.name === category.name ? 'bg-amber-600' : ''}`}
-              onClick={() => handleCategoryClick(category)}
-            >
-              {category.name}
-            </button>
-          </div>
+    <table>
+      <thead>
+        <tr>
+          <th>Category</th>
+          <th>Commands</th>
+        </tr>
+      </thead>
+      <tbody>
+        {categories.map((category, index) => (
+          <tr key={index}>
+            <td>{category.name}</td>
+            <td>
+              <ul>
+                {category.commands.map((command, index) => (
+                  <li key={index}>{command.name}: {command.description}</li>
+                ))}
+              </ul>
+            </td>
+          </tr>
         ))}
-      </div>
-
-      <div className="flex flex-wrap mt-10">
-        {selectedCategory.commands.map((command) => (
-          <div key={command.name} className="w-full md:w-1/2 lg:w-1/3 px-2 mb-4">
-            <div className="h-full bg-white shadow-md rounded-md px-4 py-6 hover:shadow-lg transition duration-200">
-              <h2 className="text-lg font-bold mb-2">{command.name}</h2>
-              <p className="text-gray-600">{command.description}</p>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
+      </tbody>
+    </table>
   );
-}
+};
+
+export default Table;
