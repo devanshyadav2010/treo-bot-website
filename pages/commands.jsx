c20bdb0613b6b5380956f7b915e273fd7e98348e
Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>
2023-04-08 15:57:39 +0530

    Update commands.jsx
    Signed-off-by: Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>

--- pages/commands.jsx
+++ pages/commands.jsx
@@ -1,70 +1,84 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-const commands = [
-  { name: "/help", category: "Information" },
-  { name: "/invite", category: "Utility" },
-  { name: "/lyrics", category: "Utility" },
-  { name: "/ping", category: "Information" },
-  { name: "/stats", category: "Information" },
-  { name: "/setdj", category: "Utility" },
-  { name: "/play", category: "Utility" },
-  { name: "/search", category: "Utility" },
-  { name: "/stop", category: "Utility" },
-  { name: "/skip", category: "Utility" },
-  { name: "/back", category: "Utility" },
-  { name: "/clear", category: "Utility" },
-  { name: "/time", category: "Utility" },
-  { name: "/queue", category: "Utility" },
-  { name: "/pause", category: "Utility" },
-  { name: "/resume", category: "Utility" },
-  { name: "/volume", category: "Utility" },
-  { name: "/nowplaying", category: "Utility" },
-  { name: "/save", category: "Utility" },
-  { name: "/loop", category: "Utility" },
-  { name: "/filter", category: "Utility" },
+const categories = [  {    name: 'Information',    commands: [      { name: 'help' },      { name: 'user' },    ],
+  },
+  {
+    name: 'Economy',
+    commands: [
+      { name: 'balance' },
+      { name: 'shop' },
+    ],
+  },
+  {
+    name: 'Utility',
+    commands: [
+      { name: 'poll' },
+      { name: 'remind' },
+    ],
+  },
+  {
+    name: 'Antinuke',
+    commands: [
+      { name: 'ban' },
+      { name: 'kick' },
+    ],
+  },
+  {
+    name: 'Ticket',
+    commands: [
+      { name: 'new' },
+      { name: 'close' },
+    ],
+  },
+  {
+    name: 'Welcome',
+    commands: [
+      { name: 'greet' },
+      { name: 'goodbye' },
+    ],
+  },
 ];
 
-export default function Commands() {
-  const [selectedCategory, setSelectedCategory] = useState("All");
+export default function CommandPage() {
+  const [selectedCategoryIndex, setSelectedCategoryIndex] = useState(0);
 
-  const categories = ["All", "Information", "Economy", "Utility", "Antinuke", "Ticket", "Welcome"];
-
-  const filteredCommands = selectedCategory === "All" ? commands : commands.filter((c) => c.category === selectedCategory);
+  const handleClick = (index) => {
+    setSelectedCategoryIndex(index);
+  };
 
   return (
-    <div className="flex flex-col items-center">
-      <h1 className="text-3xl font-bold my-8">Bot Commands</h1>
-      <div className="flex space-x-4">
+    <div>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
         {categories.map((category, index) => (
-          <button
-            key={index}
-            className={`px-4 py-2 rounded-md border border-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 ${
-              category === selectedCategory ? "bg-blue-500 text-white" : "bg-white text-gray-700"
-            }`}
-            onClick={() => setSelectedCategory(category)}
-          >
-            {category}
+          <button key={index} onClick={() => handleClick(index)} style={{ margin: '5px' }}>
+            {category.name}
           </button>
         ))}
       </div>
-      <div className="mt-8 w-full">
-        <table className="w-full border-collapse">
-          <thead className="bg-gray-50">
-            <tr>
-              <th className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">Command</th>
-              <th className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">Category</th>
-            </tr>
-          </thead>
-          <tbody className="bg-white divide-y divide-gray-200">
-            {filteredCommands.map((command, index) => (
-              <tr key={index}>
-                <td className="px-6 py-4 whitespace-nowrap">{command.name}</td>
-                <td className="px-6 py-4 whitespace-nowrap capitalize">{command.category}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        {categories[selectedCategoryIndex].commands.map((command, index) => (
+          <button key={index} style={{ margin: '5px' }}>
+            {command.name}
+          </button>
+        ))}
       </div>
+      <table style={{ margin: 'auto', marginTop: '20px', borderCollapse: 'collapse' }}>
+        <thead>
+          <tr>
+            <th>Category Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {categories.map((category, index) => (
+            <tr key={index}>
+              <td>{category.name}</td>
+              {category.commands.map((command, index) => (
+                <td key={index}>{command.name}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
