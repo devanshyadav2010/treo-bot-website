bf49f3ca0923a4769a2283407b1d427f995fd2aa
Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>
2023-04-08 20:16:26 +0530

    Update commands.jsx
    Signed-off-by: Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>

--- pages/commands.jsx
+++ pages/commands.jsx
@@ -1,95 +1,44 @@
 import { useState } from 'react';
-
-const categories = [
-  {
-    name: "Information",
-    commands: [
-      {
-        name: "Help",
-        description: "Get help with using the bot."
-      },
-      {
-        name: "User",
-        description: "Get information about a user."
-      }
-    ]
-  },
-  {
-    name: "Economy",
-    commands: [
-      {
-        name: "Servers",
-        description: "Get information about the servers the bot is in."
-      },
-      {
-        name: "Members",
-        description: "Get information about the members in a server."
-      }
-    ]
-  },
-  {
-    name: "Utility",
-    commands: [
-      {
-        name: "Ping",
-        description: "Get the latency of the bot."
-      },
-      {
-        name: "Poll",
-        description: "Create a poll for members to vote on."
-      }
-    ]
-  },
-  {
-    name: "Antinuke",
-    commands: [
-      {
-        name: "Ban",
-        description: "Ban a user from a server."
-      },
-      {
-        name: "Kick",
-        description: "Kick a user from a server."
-      }
-    ]
-  },
-  {
-    name: "Ticket",
-    commands: [
-      {
-        name: "Open",
-        description: "Open a support ticket."
-      },
-      {
-        name: "Close",
-        description: "Close a support ticket."
-      }
-    ]
-  },
-  {
-    name: "Welcome",
-    commands: [
-      {
-        name: "Set",
-        description: "Set the welcome message for a server."
-      },
-      {
-        name: "Reset",
-        description: "Reset the welcome message for a server."
-      }
-    ]
-  }
-];
+import { AiOutlineSearch } from 'react-icons/ai';
+import categories from '../data/categories';
 
 export default function Commands() {
   const [selectedCategory, setSelectedCategory] = useState(categories[0]);
+  const [searchText, setSearchText] = useState('');
 
   const handleCategoryClick = (category) => {
     setSelectedCategory(category);
-  }
+  };
+
+  const handleSearchTextChange = (event) => {
+    setSearchText(event.target.value);
+  };
+
+  const filteredCommands = selectedCategory.commands.filter((command) =>
+    command.name.toLowerCase().includes(searchText.toLowerCase())
+  );
 
   return (
     <div className="container mx-auto mt-10">
+      <div className="flex flex-col md:flex-row md:justify-between items-center">
+        <h1 className="text-4xl font-bold text-gray-900 mb-4">Commands</h1>
+        <div className="w-full md:w-1/3 mb-4 md:mb-0">
+          <div className="relative rounded-md shadow-sm">
+            <input
+              type="search"
+              name="search"
+              id="search"
+              value={searchText}
+              onChange={handleSearchTextChange}
+              className="focus:ring-amber-500 focus:border-amber-500 block w-full pl-10 sm:text-sm border-gray-300 rounded-md"
+              placeholder="Search commands"
+            />
+            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+              <AiOutlineSearch className="h-5 w-5 text-gray-400" />
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="flex flex-wrap flex-col md:flex-row">
         {categories.map((category) => (
           <div key={category.name} className="md:w-1/3 md:px-2 mb-4">
@@ -104,7 +53,7 @@ export default function Commands() {
       </div>
 
       <div className="flex flex-wrap mt-10">
-        {selectedCategory.commands.map((command) => (
+        {filteredCommands.map((command) => (
           <div key={command.name} className="w-full md:w-1/2 lg:w-1/3 px-2 mb-4">
             <div className="h-full bg-white shadow-md rounded-md px-4 py-6 hover:shadow-lg transition duration-200">
               <h2 className="text-lg font-bold mb-2">{command.name}</h2>
