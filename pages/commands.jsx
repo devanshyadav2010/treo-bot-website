4ee8fb6efe82f632ecf161fda2c28dd2f6e4b47e
Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>
2023-04-08 19:38:58 +0530

    Update commands.jsx
    Signed-off-by: Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>

--- pages/commands.jsx
+++ pages/commands.jsx
@@ -1,126 +1,118 @@
 import { useState } from 'react';
 
-const categories = [  {    name: 'Information',    commands: [      {        name: '!help',        description: 'Displays the list of available commands',      },      {        name: '!about',        description: 'Displays information about the bot',      },      {        name: '!serverinfo',        description: 'Displays information about the current server',      },    ],
-  },
+const categories = [
   {
-    name: 'Economy',
+    name: "Information",
     commands: [
       {
-        name: '!balance',
-        description: 'Displays your current balance',
+        name: "Help",
+        description: "Get help with using the bot."
       },
       {
-        name: '!daily',
-        description: 'Gives you your daily reward',
-      },
-      {
-        name: '!pay',
-        description: 'Pays a user from your balance',
-      },
-    ],
+        name: "User",
+        description: "Get information about a user."
+      }
+    ]
   },
   {
-    name: 'Utility',
+    name: "Economy",
     commands: [
       {
-        name: '!poll',
-        description: 'Creates a poll',
+        name: "Servers",
+        description: "Get information about the servers the bot is in."
       },
       {
-        name: '!weather',
-        description: 'Displays the current weather',
-      },
-      {
-        name: '!remindme',
-        description: 'Sets a reminder for you',
-      },
-    ],
+        name: "Members",
+        description: "Get information about the members in a server."
+      }
+    ]
   },
   {
-    name: 'Antinuke',
+    name: "Utility",
     commands: [
       {
-        name: '!antiraid on',
-        description: 'Turns on the antiraid system',
+        name: "Ping",
+        description: "Get the latency of the bot."
       },
       {
-        name: '!antiraid off',
-        description: 'Turns off the antiraid system',
-      },
-    ],
+        name: "Poll",
+        description: "Create a poll for members to vote on."
+      }
+    ]
   },
   {
-    name: 'Ticket',
+    name: "Antinuke",
     commands: [
       {
-        name: '!newticket',
-        description: 'Opens a new ticket',
+        name: "Ban",
+        description: "Ban a user from a server."
       },
       {
-        name: '!closeticket',
-        description: 'Closes the current ticket',
-      },
-    ],
+        name: "Kick",
+        description: "Kick a user from a server."
+      }
+    ]
   },
   {
-    name: 'Welcome',
+    name: "Ticket",
     commands: [
       {
-        name: '!welcome on',
-        description: 'Turns on the welcome system',
+        name: "Open",
+        description: "Open a support ticket."
       },
       {
-        name: '!welcome off',
-        description: 'Turns off the welcome system',
-      },
-    ],
+        name: "Close",
+        description: "Close a support ticket."
+      }
+    ]
   },
+  {
+    name: "Welcome",
+    commands: [
+      {
+        name: "Set",
+        description: "Set the welcome message for a server."
+      },
+      {
+        name: "Reset",
+        description: "Reset the welcome message for a server."
+      }
+    ]
+  }
 ];
 
 export default function Commands() {
-  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  const [selectedCategory, setSelectedCategory] = useState(categories[0]);
 
-  function handleCategoryClick(category) {
-    setCurrentCategory(category);
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
   }
 
   return (
-    <div className="py-10">
-      <div className="flex justify-center gap-4 mb-8">
+    <div className="container mx-auto mt-10">
+      <div className="flex flex-wrap flex-col md:flex-row">
         {categories.map((category) => (
-          <button
-            key={category.name}
-            className={`${
-              currentCategory === category
-                ? 'bg-indigo-600 text-white'
-                : 'bg-white text-indigo-600'
-            } px-4 py-2 rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2`}
-            onClick={() => handleCategoryClick(category)}
-          >
-            {category.name}
-          </button>
+          <div key={category.name} className="md:w-1/3 md:px-2 mb-4">
+            <button
+              className={`w-full md:w-auto h-12 shadow-lg shadow-amber-600/20 rounded-md py-2 px-2 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white ${selectedCategory.name === category.name ? 'bg-amber-600' : ''}`}
+              onClick={() => handleCategoryClick(category)}
+            >
+              {category.name}
+            </button>
+          </div>
         ))}
       </div>
-      <div className="max-w-2xl mx-auto">
-        <table className="table-auto w-full">
-          <thead>
-            <tr>
-              <th className="px-4 py-2">Command</th>
-              <th className="px-4 py-2">Description</th>
-            </tr>
-          </thead>
-          <tbody>
-            {currentCategory.commands.map((command) => (
-              <tr key={command.name}>
-                <td className="border px-4 py-2">{command.name}</td>
-                <td className="border px-4 py-2">{command.description}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+
+      <div className="flex flex-wrap mt-10">
+        {selectedCategory.commands.map((command) => (
+          <div key={command.name} className="w-full md:w-1/2 lg:w-1/3 px-2 mb-4">
+            <div className="h-full bg-white shadow-md rounded-md px-4 py-6 hover:shadow-lg transition duration-200">
+              <h2 className="text-lg font-bold mb-2">{command.name}</h2>
+              <p className="text-gray-600">{command.description}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
 }
-
