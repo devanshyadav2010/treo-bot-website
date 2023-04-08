3fad9936f3714d3a04028dc7d01984771a3f1020
Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>
2023-04-08 19:22:59 +0530

    Update commands.jsx
    Signed-off-by: Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>

--- pages/commands.jsx
+++ pages/commands.jsx
@@ -1,118 +1,116 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-const categories = [
-  {
-    name: "Information",
-    commands: [
-      {
-        name: "Help",
-        description: "Get help with using the bot."
-      },
-      {
-        name: "User",
-        description: "Get information about a user."
-      }
-    ]
+const commandCategories = [  {    name: "Information",    description: "Commands that provide information about the server or its members.",    commands: [      {        name: "!help",        description: "Display a list of available commands.",      },      {        name: "!info",        description: "Display information about the server.",      },      {        name: "!rules",        description: "Display the server's rules.",      },    ],
   },
   {
     name: "Economy",
+    description: "Commands that relate to the server's economy or currency system.",
     commands: [
       {
-        name: "Servers",
-        description: "Get information about the servers the bot is in."
+        name: "!balance",
+        description: "Display your account balance.",
       },
       {
-        name: "Members",
-        description: "Get information about the members in a server."
-      }
-    ]
+        name: "!shop",
+        description: "Display the server's shop.",
+      },
+      {
+        name: "!buy [item]",
+        description: "Buy an item from the server's shop.",
+      },
+    ],
   },
   {
     name: "Utility",
+    description: "Commands that provide utility functions, such as time zone conversions or weather reports.",
     commands: [
       {
-        name: "Ping",
-        description: "Get the latency of the bot."
+        name: "!timezone [location]",
+        description: "Display the current time in a specified time zone.",
       },
       {
-        name: "Poll",
-        description: "Create a poll for members to vote on."
-      }
-    ]
+        name: "!weather [location]",
+        description: "Display the current weather in a specified location.",
+      },
+    ],
   },
   {
     name: "Antinuke",
+    description: "Commands that protect the server from malicious actions or spam.",
     commands: [
       {
-        name: "Ban",
-        description: "Ban a user from a server."
+        name: "!ban [user] [reason]",
+        description: "Ban a user from the server.",
       },
       {
-        name: "Kick",
-        description: "Kick a user from a server."
-      }
-    ]
+        name: "!kick [user] [reason]",
+        description: "Kick a user from the server.",
+      },
+      {
+        name: "!mute [user] [duration] [reason]",
+        description: "Mute a user for a specified duration.",
+      },
+    ],
   },
   {
     name: "Ticket",
+    description: "Commands that create or manage support tickets.",
     commands: [
       {
-        name: "Open",
-        description: "Open a support ticket."
+        name: "!newticket [issue]",
+        description: "Create a new support ticket with a specified issue.",
       },
       {
-        name: "Close",
-        description: "Close a support ticket."
-      }
-    ]
+        name: "!closeticket [ticket]",
+        description: "Close a support ticket with a specified ID.",
+      },
+    ],
   },
   {
     name: "Welcome",
+    description: "Commands that welcome new users to the server or provide introductions.",
     commands: [
       {
-        name: "Set",
-        description: "Set the welcome message for a server."
+        name: "!welcome [user]",
+        description: "Send a welcome message to a new user.",
       },
       {
-        name: "Reset",
-        description: "Reset the welcome message for a server."
-      }
-    ]
-  }
+        name: "!intro",
+        description: "Introduce yourself to the server.",
+      },
+    ],
+  },
 ];
 
 export default function Commands() {
-  const [selectedCategory, setSelectedCategory] = useState(categories[0]);
-
-  const handleCategoryClick = (category) => {
-    setSelectedCategory(category);
-  }
+  const [currentCategory, setCurrentCategory] = useState(commandCategories[0]);
 
   return (
-    <div className="container mx-auto mt-10">
-      <div className="flex flex-wrap flex-col md:flex-row">
-        {categories.map((category) => (
-          <div key={category.name} className="md:w-1/3 md:px-2 mb-4">
-            <button
-              className={`w-full md:w-auto h-12 shadow-lg shadow-amber-600/20 rounded-md py-2 px-2 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white ${selectedCategory.name === category.name ? 'bg-amber-600' : ''}`}
-              onClick={() => handleCategoryClick(category)}
-            >
-              {category.name}
-            </button>
-          </div>
+    <div>
+      <div className="py-10 flex justify-center">
+        {commandCategories.map((category) => (
+          <button
+            key={category.name}
+            className={`mx-2 px-6 rounded-md py-2 font-medium ${
+              currentCategory.name === category.name
+                ? "bg-gradient-to-bl from-amber-700 to-amber-500 text-white shadow-lg shadow-amber-600/20"
+                : "bg-gray-200 text-gray-600 hover:bg-gray-300"
+            }`}
+            onClick={() => setCurrentCategory(category)}
+          >
+            {category.name}
+          </button>
         ))}
       </div>
-
-      <div className="flex flex-wrap mt-10">
-        {selectedCategory.commands.map((command) => (
-          <div key={command.name} className="w-full md:w-1/2 lg:w-1/3 px-2 mb-4">
-            <div className="h-full bg-white shadow-md rounded-md px-4 py-6 hover:shadow-lg transition duration-200">
-              <h2 className="text-lg font-bold mb-2">{command.name}</h2>
-              <p className="text-gray-600">{command.description}</p>
-            </div>
+      <div className="px-10">
+        <h2 className="text-3 {currentCategory.commands.map((command) => (
+          <div key={command.name} className="py-2">
+            <div className="text-lg font-medium">{command.name}</div>
+            <div className="text-sm text-gray-500">{command.description}</div>
           </div>
         ))}
       </div>
     </div>
   );
 }
+
