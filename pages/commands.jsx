c22f99d8499d85b28269214bc666ba8864c1dfe0
Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>
2023-04-08 17:07:56 +0530

    Update commands.jsx
    Signed-off-by: Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>

--- pages/commands.jsx
+++ pages/commands.jsx
@@ -1,18 +1,6 @@
 import { useState } from 'react';
 
-const categories = [
-  {
-    name: "Information",
-    commands: [
-      {
-        name: "Help",
-        description: "Get help with using the bot."
-      },
-      {
-        name: "User",
-        description: "Get information about a user."
-      }
-    ]
+const categories = [  {    name: "Information",    commands: [      {        name: "Help",        description: "Get help with using the bot."      },      {        name: "User",        description: "Get information about a user."      }    ]
   },
   {
     name: "Economy",
@@ -90,11 +78,11 @@ export default function Commands() {
 
   return (
     <div className="container mx-auto mt-10">
-      <div className="flex flex-wrap">
+      <div className="flex flex-wrap flex-col md:flex-row">
         {categories.map((category) => (
-          <div key={category.name} className="w-full md:w-1/2 lg:w-1/3 px-2 mb-4">
+          <div key={category.name} className="w-full md:w-1/3 px-2 mb-4">
             <button
-              className={`w-full shadow-lg shadow-amber-600/20 rounded-md py-2 px-2 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white ${selectedCategory.name === category.name ? 'bg-amber-600' : ''}`}
+              className={`w-full h-12 shadow-lg shadow-amber-600/20 rounded-md py-2 px-2 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white ${selectedCategory.name === category.name ? 'bg-amber-600' : ''}`}
               onClick={() => handleCategoryClick(category)}
             >
               {category.name}
@@ -106,13 +94,13 @@ export default function Commands() {
       <div className="flex flex-wrap mt-10">
         {selectedCategory.commands.map((command) => (
           <div key={command.name} className="w-full md:w-1/2 lg:w-1/3 px-2 mb-4">
-            <div className="bg-white border border-gray-200 rounded-lg p-4 h-full">
-              <div className="font-medium mb-2">{command.name}</div>
-              <div className="text-gray-500">{command.description}</div>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+            <div className="h-full bg-white shadow-md rounded-md px-4 py-6 hover:shadow-lg transition duration-200">
+<h2 className="text-lg font-bold mb-2">{command.name}</h2>
+<p className="text-gray-600">{command.description}</p>
+</div>
+</div>
+))}
+</div>
+</div>
+);
 }
