2e930b440e73a8b9189fa34287fe5d422bdc90b7
Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>
2023-04-08 15:51:20 +0530

    Update commands.jsx
    Signed-off-by: Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>

--- pages/commands.jsx
+++ pages/commands.jsx
@@ -1,110 +1,71 @@
-import { Menu, Transition } from "@headlessui/react";
-import Link from "next/link";
+import { useState } from 'react';
+
+const commands = [
+  { name: '/help', category: 'Information' },
+  { name: '/invite', category: 'Information' },
+  { name: '/lyrics', category: 'Information' },
+  { name: '/ping', category: 'Utility' },
+  { name: '/stats', category: 'Utility' },
+  { name: '/setdj', category: 'Utility' },
+  { name: '/play', category: 'Utility' },
+  { name: '/search', category: 'Utility' },
+  { name: '/stop', category: 'Utility' },
+  { name: '/skip', category: 'Utility' },
+  { name: '/back', category: 'Utility' },
+  { name: '/clear', category: 'Utility' },
+  { name: '/time', category: 'Utility' },
+  { name: '/queue', category: 'Utility' },
+  { name: '/pause', category: 'Utility' },
+  { name: '/resume', category: 'Utility' },
+  { name: '/volume', category: 'Utility' },
+  { name: '/nowplaying', category: 'Utility' },
+  { name: '/save', category: 'Utility' },
+  { name: '/loop', category: 'Utility' },
+  { name: '/filter', category: 'Utility' },
+  { name: '/antinuke', category: 'Antinuke' },
+  { name: '/newticket', category: 'Ticket' },
+  { name: '/welcome', category: 'Welcome' }
+];
+
+const CommandTable = ({ category }) => {
+  const filteredCommands = commands.filter(cmd => cmd.category === category);
+  return (
+    <table className="table-auto">
+      <thead>
+        <tr>
+          <th className="px-4 py-2">Name</th>
+        </tr>
+      </thead>
+      <tbody>
+        {filteredCommands.map(cmd => (
+          <tr key={cmd.name}>
+            <td className="border px-4 py-2">{cmd.name}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 export default function Commands() {
-  const commands = [
-    "/help",
-    "/invite",
-    "/lyrics",
-    "/ping",
-    "/stats",
-    "/setdj",
-    "/play",
-    "/serach",
-    "/stop",
-    "/skip",
-    "/back",
-    "/clear",
-    "/time",
-      "/queue",
-      "/pause",
-      "/resume",
-      "/volume",
-      "/nowplaying",
-      "/save",
-      "/loop",
-      "/filter"
-      ]
+  const [activeCategory, setActiveCategory] = useState('Information');
+
   return (
-    <>
+    <div className="max-w-4xl mx-auto my-10">
+      <div className="flex items-center mb-4">
+        {['Information', 'Economy', 'Utility', 'Antinuke', 'Ticket', 'Welcome'].map(cat => (
+          <button
+            key={cat}
+            className={`mr-4 px-4 py-2 rounded-lg ${activeCategory === cat ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'}`}
+            onClick={() => setActiveCategory(cat)}
+          >
+            {cat}
+          </button>
+        ))}
+      </div>
       <div>
-      <div className="flex w-full justify-center">
-      <img width="500" src="/img/bck2.png" />
+        <CommandTable category={activeCategory} />
       </div>
-        <p className="text-xl font-medium text-white">
-          <i className="fal fa-cogs text-amber-400 mr-2" />
-         Bot Commands
-        </p>
-        <p className="text-white text-sm text-opacity-50 mb-5">
-        You can get information about the commands of the Astra Bot.
-        </p>
-
-        <div>
-          <Menu as="div" className="w-full relative inline-block text-left">
-          <div>
-              <Menu.Button className="left-0 transition w-full duration-200 hover:bg-opacity-50 bg-black bg-opacity-30 text-white focus:text-amber-400 rounded-xl border border-white/10 focus:border-amber-400 focus:outline-none py-4 px-6 flex justify-between items-center">
-                <div className="flex items-center gap-x-1 text-white text-opacity-60">
-                 
-                    <>
-                      <i className="fa fa-hashtag text-white text-opacity-20" />
-                     View Commands List
-                    </>
-                
-                </div>
-              </Menu.Button>
-            </div>
-
-            <Transition
-              enter="transition ease-out duration-100"
-              enterFrom="transform opacity-0 scale-95"
-              enterTo="transform opacity-100 scale-100"
-              leave="transition ease-in duration-75"
-              leaveFrom="transform opacity-100 scale-100"
-              leaveTo="transform opacity-0 scale-95">
-              <Menu.Items className="z-1 custom-scroll absolute left-0 w-full mt-2 origin-top-right bg-black overflow-auto max-h-60 border border-black border-opacity-20 bg-opacity-95 rounded-lg p-1">
-                <div className="px-1 py-1 w-full">
-
-            {commands.map(astra => (
-
-                <Menu.Item>
-                    <button className={`w-full rounded-lg p-2 cursor-pointer text-white flex items-center space-x-3 transition-all duration-200`}>
-                    <>
-                          <i className="fa-spin text-xl text-white text-opacity-20" />
-                         {astra}</>
-                    </button>
-                    </Menu.Item>
-            ))}
-              
-        </div>
-              </Menu.Items>
-            </Transition>
-          </Menu>
-        </div>
-
-        <div className="animateHeader mt-10 flex flex-wrap items-center justify-center gap-x-4">
-  
-  <Link href={"https://bit.ly/3PHDjyC"}>
-  <a className={"flex items-center px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
-Invite Astra</a>
-  </Link>
-
-  <div className="py-10"></div>
-  
-  <Link href={"https://discord.gg/ZcDprCrFTn"}>
-  <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
-Support Server</a>
-  </Link>
-
-  <div className="py-10"></div>
-  
-  <Link href={"https://bit.ly/3LYzaDe"}>
-  <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
-Vote Treo</a>
-  </Link>
-</div>
-
-      </div>
-      <div className="py-20"></div>
-    </>
-  )
-  }
+    </div>
+  );
+}
