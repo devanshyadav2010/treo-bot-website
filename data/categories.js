40c139471a0aa26480147ad0f6cb8163d5f924f6
Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>
2023-04-09 23:00:08 +0530

    Update categories.js
    Signed-off-by: Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>

--- data/categories.js
+++ data/categories.js
@@ -1,42 +1,42 @@
-const categories = [  {    name: "General",    commands: [      {        name: "help",        description: "Displays a list of all available commands.",        usage: "!help"      },      {        name: "ping",        description: "Checks the bot's latency.",        usage: "!ping"      },      {        name: "info",        description: "Displays some information about the bot.",        usage: "!info"      }    ]
+const categories = [  {    name: "All",    commands: [      {        name: "help",        description: "Displays a list of all available commands.",        usage: "/help"      },      {        name: "ping",        description: "Checks the bot's latency.",        usage: "!ping"      },      {        name: "info",        description: "Displays some information about the bot.",        usage: "!info"      }    ]
+  },
+  {
+    name: "Antinuke",
+    commands: [
+      {
+        name: "Soon",
+        description: "Soon",
+        usage: "Soon"
+      },
+      {
+        name: "Soon",
+        description: "Soon",
+        usage: "Soon"
+      },
+      {
+        name: "Soon",
+        description: "Soon",
+        usage: "Soon"
+      }
+    ]
   },
   {
     name: "Moderation",
     commands: [
       {
-        name: "kick",
-        description: "Kicks a user from the server.",
-        usage: "!kick <@user> <reason>"
+        name: "Soon",
+        description: "Soon",
+        usage: "Soon"
       },
       {
-        name: "ban",
-        description: "Bans a user from the server.",
-        usage: "!ban <@user> <reason>"
+        name: "Soon",
+        description: "Soon",
+        usage: "Soon"
       },
       {
-        name: "mute",
-        description: "Mutes a user in the server.",
-        usage: "!mute <@user> <duration> <reason>"
-      }
-    ]
-  },
-  {
-    name: "Fun",
-    commands: [
-      {
-        name: "meme",
-        description: "Displays a random meme from a selection of subreddits.",
-        usage: "!meme"
-      },
-      {
-        name: "cat",
-        description: "Displays a random cat picture.",
-        usage: "!cat"
-      },
-      {
-        name: "dog",
-        description: "Displays a random dog picture.",
-        usage: "!dog"
+        name: "Soon",
+        description: "Soon",
+        usage: "Soon"
       }
     ]
   }
