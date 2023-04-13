40b924f6818c07937ef70368afbc1987bf1040c1
Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>
2023-04-13 23:15:32 +0530

    Create changelogs.jsx
    Signed-off-by: Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>

---
+++ pages/changelogs.jsx
@@ -0,0 +1,37 @@
+
+import React, { useState } from 'react';
+import fs from 'fs';
+
+export default function Changelogs() {
+  const [changelogs, setChangelogs] = useState([]);
+
+  // read the changelog file on page load
+  useEffect(() => {
+    const changelogData = fs.readFileSync('./changelog.json', 'utf-8');
+    setChangelogs(JSON.parse(changelogData));
+  }, []);
+
+  return (
+    <div className="changelogs-page">
+      <header>
+        <h1>Changelogs</h1>
+      </header>
+      <main>
+        {changelogs.map(changelog => (
+          <div className="changelog-item" key={changelog.version}>
+            <h2>{changelog.version}</h2>
+            <small>{changelog.date}</small>
+            <ul>
+              {changelog.changes.map(change => (
+                <li key={change}>{change}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </main>
+      <style jsx>{`
+        /* Add CSS styling here */
+      `}</style>
+    </div>
+  );
+}
