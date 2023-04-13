e1d58a7b2e322ebbc4129bab2ca04dc3c2a144e6
Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>
2023-04-13 23:22:15 +0530

    Update changelogs.jsx
    Signed-off-by: Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>

--- pages/changelogs.jsx
+++ pages/changelogs.jsx
@@ -1,37 +1,25 @@
-
-import React, { useState } from 'react';
-import fs from 'fs';
+import React from 'react';
 
 export default function Changelogs() {
-  const [changelogs, setChangelogs] = useState([]);
-
-  // read the changelog file on page load
-  useEffect(() => {
-    const changelogData = fs.readFileSync('../data/changelogs.json', 'utf-8');
-    setChangelogs(JSON.parse(changelogData));
-  }, []);
-
   return (
-    <div className="changelogs-page">
-      <header>
-        <h1>Changelogs</h1>
-      </header>
-      <main>
-        {changelogs.map(changelog => (
-          <div className="changelog-item" key={changelog.version}>
-            <h2>{changelog.version}</h2>
-            <small>{changelog.date}</small>
-            <ul>
-              {changelog.changes.map(change => (
-                <li key={change}>{change}</li>
-              ))}
-            </ul>
-          </div>
-        ))}
-      </main>
-      <style jsx>{`
-        /* Add CSS styling here */
-      `}</style>
+    <div>
+      <h1>Changelog</h1>
+      <ul>
+        <li>
+          <h2>Version 1.0.0</h2>
+          <ul>
+            <li>Added new feature A</li>
+            <li>Fixed bug B</li>
+          </ul>
+        </li>
+        <li>
+          <h2>Version 1.1.0</h2>
+          <ul>
+            <li>Added new feature C</li>
+            <li>Improved performance</li>
+          </ul>
+        </li>
+      </ul>
     </div>
   );
 }
