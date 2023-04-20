e9065f073720694debf217aa0448d3fd6476949a
Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>
2023-04-21 01:04:12 +0530

    Update commands.jsx
    Signed-off-by: Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>

--- pages/commands.jsx
+++ pages/commands.jsx
@@ -74,22 +74,20 @@ export default function Commands() {
           </div>
         ))}
       </div>
-
-      <Modal isOpen={selectedCommand !== null} onRequestClose={handleCloseModal} className="modal">
-        <div className="flex justify-between items-center mb-4">
-          <h2 
-
-          {selectedCommand && (
-            <>
-              <h2 className="text-2xl font-bold text-gray-900">{selectedCommand.name}</h2>
-              <button onClick={handleCloseModal} className="text-gray-500 hover:text-gray-700 focus:outline-none">
-                <span className="sr-only">Close</span>
-                <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-                </svg>
-              </button>
-            </>
-          )}
+<Modal isOpen={selectedCommand !== null} onRequestClose={handleCloseModal} className="modal">
+  <div className="flex justify-between items-center mb-4">
+    {selectedCommand && (
+      <>
+        <h2 className="text-2xl font-bold text-gray-900">{selectedCommand.name}</h2>
+        <button onClick={handleCloseModal} className="text-gray-500 hover:text-gray-700 focus:outline-none">
+          <span className="sr-only">Close</span>
+          <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </button>
+      </>
+    )}
+  </div>
           {selectedCommand && (
             <div className="mt-4">
               <h3 className="text-lg font-bold text-gray-900 mb-2">Usage:</h3>
