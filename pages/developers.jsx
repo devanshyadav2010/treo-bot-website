15e9a4c7f44c48a3754084c4188e6a2a3695f7d7
Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>
2023-04-25 00:44:15 +0530

    Update developers.jsx
    Signed-off-by: Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>

--- pages/developers.jsx
+++ pages/developers.jsx
@@ -34,7 +34,7 @@ export default function Developers() {
                     <div className="flex flex-col justify-center text-white rounded">
                         <a href="https://discord.gg/JaNCZvPxMQ">
                             <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
-                            <img className="rounded-full h-32 w-32" src="https://imgur.io/HQ2rOM8" />
+                            <img className="rounded-full h-32 w-32" src="https://i.imgur.com/HQ2rOM8.png" />
                             <div>
                                 <h1 className="leading-none text-3xl font-bold text-white">Heyo! I'm Toby</h1>        
                                 <div className="flex items-center mt-1">
