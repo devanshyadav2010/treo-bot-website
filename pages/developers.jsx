0b5b239dbde3d268cd75c703ba004ef30b64bfc5
Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>
2023-05-11 02:39:05 +0530

    Update developers.jsx
    Signed-off-by: Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>

--- pages/developers.jsx
+++ pages/developers.jsx
@@ -1,24 +1,29 @@
+
+import { FaStars } from 'react-icons/fa';
+
 export default function Developers() {
   return (
-    <>
-      <div className="w-full my-10">
-        <div className="flex w-full justify-center">
-          <img width="400" src="/img/bck4.png" />
-        </div>
-        <h1 className="py-7 animateHeader text-4xl font-extrabold text-center text-white">
-          <i className="fal fa-stars text-amber-400 mr-2" />
-          Developers
-        </h1>
-
-        <div className="flex justify-center">
-          <img src="https://discord.c99.nl/widget/theme-2/980069165459198022.png" alt="Discord Widget" />
-        </div>
-        <div className="flex justify-center">
-          <img src="https://discord.c99.nl/widget/theme-2/903237169722834954.png" alt="Discord Widget" />
-        </div>
-
+    <div className="container mx-auto mt-10">
+      <div className="flex justify-center">
+        <img className="w-64" src="/img/bck4.png" alt="Background" />
       </div>
-      <div className="py-10"></div>
-    </>
+      <h1 className="py-7 text-4xl font-extrabold text-center text-white">
+        <FaStars className="text-amber-400 mr-2 text-6xl inline-block" />
+        Developers
+      </h1>
+
+      <div className="flex justify-center">
+        <DiscordWidget imageUrl="https://discord.c99.nl/widget/theme-2/980069165459198022.png" />
+      </div>
+      <div className="flex justify-center">
+        <DiscordWidget imageUrl="https://discord.c99.nl/widget/theme-2/903237169722834954.png" />
+      </div>
+    </div>
   );
-};
+}
+
+function DiscordWidget({ imageUrl }) {
+  return (
+    <img className="w-96 shadow-lg rounded-lg" src={imageUrl} alt="Discord Widget" />
+  );
+}
