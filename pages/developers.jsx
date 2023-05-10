03246f6086eb83ddf82acf88bceb0020e96d761e
Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>
2023-05-11 02:46:59 +0530

    Update developers.jsx
    Signed-off-by: Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>

--- pages/developers.jsx
+++ pages/developers.jsx
@@ -1,74 +1,27 @@
 export default function Developers() {
   return (
-    <div
-      className="container"
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        marginTop: "10px"
-      }}
-    >
-      <div
-        className="header"
-        style={{
-          position: "relative",
-          marginBottom: "20px",
-          textAlign: "center"
-        }}
-      >
-        <img
-          className="background-img"
-          src="/img/bck4.png"
-          alt="Background"
-          style={{
-            width: "400px"
-          }}
-        />
-        <h1
-          className="title"
-          style={{
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            fontSize: "48px",
-            fontWeight: "bold",
-            color: "#fff",
-            textShadow: "2px 2px 4px rgba(0, 0, 0, 0.3)"
-          }}
-        >
-          <span style={{ color: "#FBBF24", marginRight: "10px" }}>★</span>
+    <div className="container mx-auto mt-10">
+      <div className="flex justify-center">
+        <img className="w-64" src="/img/bck4.png" alt="Background" />
+      </div>
+      <h1 className="py-7 text-4xl font-extrabold text-center text-white">
+        <i className="fal fa-stars text-amber-400 mr-2" />
           Developers
         </h1>
-      </div>
 
-      <div className="discord-widgets" style={{ display: "flex", justifyContent: "center" }}>
-        <DiscordWidget
-          imageUrl="https://discord.c99.nl/widget/theme-2/980069165459198022.png"
-          style={{ margin: "10px" }}
-        />
-        <DiscordWidget
-          imageUrl="https://discord.c99.nl/widget/theme-2/903237169722834954.png"
-          style={{ margin: "10px" }}
-        />
+      <div className="flex justify-center">
+        <DiscordWidget imageUrl="https://discord.c99.nl/widget/theme-2/980069165459198022.png" />
+      </div>
+      <div className="flex justify-center">
+        <DiscordWidget imageUrl="https://discord.c99.nl/widget/theme-2/903237169722834954.png" />
       </div>
     </div>
   );
 }
 
-function DiscordWidget({ imageUrl, style }) {
+function DiscordWidget({ imageUrl }) {
   return (
-    <img
-      className="discord-widget"
-      src={imageUrl}
-      alt="Discord Widget"
-      style={{
-        width: "400px",
-        boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
-        borderRadius: "10px",
-        ...style
-      }}
-    />
+    <img className="w-96 shadow-lg rounded-lg" src={imageUrl} alt="Discord Widget" />
   );
 }
+
