6480ec0234092e5b7e5df2ab30c77af14727fff6
Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>
2023-05-22 12:35:30 +0530

    Update tos.jsx
    Signed-off-by: Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>

--- pages/tos.jsx
+++ pages/tos.jsx
@@ -1,28 +1,54 @@
+import { useEffect } from "react";
+
 export default function Tos() {
+  useEffect(() => {
+    // Scroll to the top of the page when component mounts
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div>
-     <h2 className="mt-4 text-white font-bold text-3xl">Terms of Services</h2>
+      <h2 className="mt-4 text-white font-bold text-3xl">Terms of Services</h2>
       <p className="text-sm text-white/30 mb-1">Last Update: 20/05/2022 🆕️</p>
-  <div className="animateHeader text-white text-opacity-40 mt-5"><h2 className="mt-4 text-white font-bold text-3xl">User Agreement</h2>
-<p>⊳ By inviting the bot and using its features (commands) you are agreeing to the below-mentioned Terms and Privacy Policy (Policy) of the bot.
+      <div className="animateHeader text-white text-opacity-40 mt-5">
+        <h2 className="mt-4 text-white font-bold text-3xl">User Agreement</h2>
+        <p>
+          ⊳ By inviting the bot and using its features (commands) you are agreeing to the below-mentioned Terms and Privacy Policy (Policy) of the bot.
+        </p>
+        <p>
+          You acknowledge that you have the privilege to use the bot freely on any Discord Server (Server) you share with it, that you can invite it to any Server that you have "Manage Server" rights for and that this privilege might get revoked for you if you're subject to breaking the terms and/or policy of this bot or the Terms of Service, Privacy Policy, and/or Community Guidelines of Discord Inc.
+        </p>
+        <p>
+          Through inviting the bot, it may collect specific data as described in its Policy. The intended usage of this data is for core functionalities of the bot, such as command handling, guild-specific settings, and the Welcoming System.
+        </p>
+        <div className="py-3"></div>
+        <h2 className="mt-4 text-white font-bold text-3xl">Intended Age</h2>
+        <p>⊳ The bot may not be used by individuals under the minimal age described in Discord's Terms of Service</p>
+        <div className="py-3"></div>
+        <h2 className="mt-4 text-white font-bold text-3xl">Affiliation</h2>
+        <p>
+          ⊳ The Bot is not affiliated with, supported, or made by Discord Inc. Any direct connection to Discord or any of its Trademark objects is purely coincidental. We do not claim to have the copyright ownership of Discord's assets, trademarks, or other intellectual property.
+        </p>
+        <div className="py-3"></div>
+        <h2 className="mt-4 text-white font-bold text-3xl">Liability</h2>
+        <p>
+          ⊳ The owner of the bot may not be made liable for individuals breaking these Terms at any given time. He has faith in the end users being truthful about their information and not misusing this bot or The Services of Discord Inc in a malicious way.
+        </p>
+        <p>
+          We reserve the right to update these terms at our own discretion, giving you a 1-Week (7 days) period to opt out of these terms if you're not agreeing with the new changes. You may opt out by removing the bot from any Server you have the rights for.
+        </p>
+        <div className="py-3"></div>
+        <h2 className="mt-4 text-white font-bold text-3xl">Contact</h2>
+        <p>
+          ⊳ If you have any questions or concerns about these Terms, please contact us at{" "}
+          <a className="text-white" href="https://top.gg/bot/924325576095973426/vote">
+            official Support Discord
+          </a>{" "}
+          of the Bot. If the above-mentioned Discord Support Server
 
-You acknowledge that you have the privilege to use the bot freely on any Discord Server (Server) you share with it, that you can invite it to any Server that you have "Manage Server" rights for and that this privilege might get revoked for you, if you're subject of breaking the terms and/or policy of this bot, or the Terms of Service, Privacy Policy and/or Community Guidelines of Discord Inc.
-
-Through Inviting the bot may it collect specific data as described in its Policy. The intended usage of this data is for core functionalities of the bot such as command handling, guild-specific settings and the Welcoming System..</p>
-<div className="py-3"></div>
-  <h2 className="mt-4 text-white font-bold text-3xl">Intended Age</h2>
-<p>⊳The bot may not be used by individuals under the minimal age described in Discord's Terms of Service</p>
-<div className="py-3"></div>
-  <h2 className="mt-4 text-white font-bold text-3xl">Affilation</h2>
-<p>⊳ The Bot is not affiliated with, supported or made by Discord Inc. Any direct connection to Discord or any of its Trademark objects is purely coincidental. We do not claim to have the copyright ownership of Discord's assets, trademarks or other intellectual property.</p>
-<div className="py-3"></div>
-  <h2 className="mt-4 text-white font-bold text-3xl">Liability</h2>
-<p>⊳ The owner of the bot may not be made liable for individuals breaking these Terms at any given time. He has faith in the end users being truthfully about their information and not misusing this bot or The Services of Discord Inc in a malicious way.
-We reserve the right to update these terms at our own discretion, giving you a 1-Week (7 days) period to opt out of these terms if you're not agreeing with the new changes. You may opt out by Removing the bot from any Server you have the rights for.</p>
-<div className="py-3"></div>
-  <h2 className="mt-4 text-white font-bold text-3xl">Contact</h2>
-<p>⊳ If you have any questions or concerns about these Terms, please contact us at <a className="text-white" href="https://top.gg/bot/924325576095973426/vote">official Support Discord</a> of the Bot. If the above-mentioned Discord Support Server is not available please dm DEVANSH#4075 on Discord</p>
-</div>
+ is not available, please DM DEVANSH#4075 on Discord.
+        </p>
+      </div>
     </div>
   );
 }
